ced6aa4cde2f6ab7def7b32fb05616ce8040e1c6
Rishav Gayen <gayenrishav19@gmail.com>
2025-07-07 11:46:23 +0000

    Initial Commit

---
+++ src/data/destination.js
@@ -0,0 +1,322 @@
+export const destinations = [
+  {
+    id: 'london',
+    name: 'London',
+    imageUrl: 'https://assets.editorial.aetnd.com/uploads/2019/03/topic-london-gettyimages-760251843-feature.jpg',
+    shortDesc: 'The historical capital of Egypt',
+    activities: [
+      {
+        id: 'london_bridge',
+        name: 'London Bridge Tour',
+        imageUrl: "http://wallpapercave.com/wp/T3XddDJ.jpg",
+        selected: false
+      },
+      {
+        id: 'tower_of_london',
+        name: 'Tower of London',
+        imageUrl: 'https://cdn1.matadornetwork.com/blogs/1/2023/03/Tower-of-London.jpg',
+        selected: false
+      },
+      {
+        id: 'buckingham_palace',
+        name: 'Buckingham Palace',
+        imageUrl: 'https://www.blazetrip.com/wp-content/uploads/2019/08/buckingham-palace-complete-guide.jpg',
+        selected: false
+      },
+      {
+        id: 'local_sightseeing',
+        name: 'Local Sightseeing (e.g Trafalgar Square, etc..)',
+        imageUrl: 'https://www.thetouristtrail.org/wp-content/uploads/2019/09/London-1.jpg',
+        selected: false
+      },
+      {
+        id: 'london_eye',
+        name: 'London Eye',
+        imageUrl: 'https://jooinn.com/images/london-eye-16.jpg',
+        selected: false
+      },
+      {
+        id: 'big_ben',
+        name: 'Big Ben and Parliament House',
+        imageUrl: 'https://www.thetrainline.com/cms/media/5743/uk-london-big-ben.jpg?mode=crop&width=1080&height=1080&quality=70',
+        selected: false
+      },
+      {
+        id: 'oxford_street',
+        name: 'Oxford Street (shopping experience)',
+        imageUrl: 'https://assets.londonist.com/uploads/2015/10/oxford.jpg',
+        selected: false
+      },
+    ],
+    accommodationOptions: [
+      'Accomodation (3 Star)',
+      'Accomodation (4 Star)',
+      'Accomodation (5 Star)',
+      'Airbnb'
+    ],
+    metadata: {
+      region: 'London',
+      popular: true,
+      idealFor: ['history', 'culture']
+    }
+  },
+  {
+    id: 'paris',
+    name: 'Paris',
+    imageUrl: 'https://images.ctfassets.net/mivicpf5zews/1WRjsCMwol9Bq7xfUu6Vc0/1bd22c32ed06e4ae21964af3c483b5ee/Paris_Spring.jpg',
+    shortDesc:"Renowned for it's grand temples and valley of kings",
+    activities: [
+      {
+        id: 'louvre',
+        name: 'Louvre',
+        imageUrl: 'https://paristickets.tours/wp-content/uploads/2022/09/louvre-museum-tour.jpg',
+        selected: false
+      },
+      {
+        id: 'eiffel_tower',
+        name: 'Eiffel Tower',
+        imageUrl: 'http://cdn.history.com/sites/2/2015/04/hith-eiffel-tower-iStock_000016468972Large.jpg',
+        selected: false
+      },
+      {
+        id: 'arc_de_triomphe',
+        name: 'Arc De Triomphe',
+        imageUrl: 'https://www.parisperfect.com/blog/wp-content/uploads/2012/10/Visiting-the-Arc-de-Triomphe-in-Paris-by-Paris-Perfect1.jpg',
+        selected: false
+      },
+      {
+        id: 'champs_elyssey',
+        name: 'Champs-Élysées',
+        imageUrl: 'https://images.adsttc.com/media/images/6000/de96/63c0/17e1/3600/03ce/newsletter/1.jpg?1610669710',
+        selected: false
+      },
+      {
+        id: 'operae_garnier',
+        name: 'Operae Garnier',
+        imageUrl: 'https://www.civitatis.com/f/francia/paris/tour-misterios-opera-garnier-r32.jpg',
+        selected: false
+      },
+      {
+        id: 'notre_dame',
+        name: 'Notre Dame',
+        imageUrl: 'https://cdn.britannica.com/85/83885-050-9CDCFEA9/Notre-Dame-de-Paris-France.jpg',
+        selected: false
+      },
+      {
+        id: 'sacre_coer_monmarte',
+        name: 'Sacre Coer & Monmarte',
+        imageUrl: 'https://94.citoyens.com/wp-content/blogs.dir/2/files/2022/10/basilique-du-sacre-coeur-de-montmartre-paris-wcc-petronella-cretu-scaled.jpg',
+        selected: false
+      },{
+        id: 'seine_cruise',
+        name: 'Seine Cruise with dinner/snacks',
+        imageUrl: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/28/65/87/fa/caption.jpg?w=1200&h=-1&s=1',
+        selected: false
+      },
+      {
+        id: 'perfume_factory',
+        name: 'Visit to Perfume Factory',
+        imageUrl: 'https://www.la-provenza.es/wp-content/uploads/2022/07/grasse-fragonard-costa-azul.jpg',
+        selected: false
+      },
+      {
+        id: 'paris_food_wine',
+        name: 'Parisian Food and Wine Trail',
+        imageUrl: 'https://www.cellartours.com/wp-content/uploads/2025/01/provence-rose-1536x1017.jpg',
+        selected: false
+      },
+      {
+        id: 'paris_disneyland',
+        name: 'Paris DisneyLand',
+        imageUrl: 'https://thepointsguy.global.ssl.fastly.net/us/originals/2022/11/Disneyland-Paris-Castle-scaled.jpg',
+        selected: false
+      }
+    ],
+    accommodationOptions: [
+      'Accomodation (3 Star)',
+      'Accomodation (4 Star)',
+      'Accomodation (5 Star)',
+      'Seafacing Villa',
+      'Airbnb'
+    ],
+    metadata: {
+      region: 'Paris',
+      popular: true,
+      idealFor: ['romance', 'photography']
+    }
+  },
+
+  {
+    id: 'zurich',
+    name: 'Zurich',
+    imageUrl: 'https://www.roadaffair.com/wp-content/uploads/2018/09/fraumunster-church-zurich-switzerland-shutterstock_322193348.jpg',
+    shortDesc: 'Serene Nile Views and ancient Nubian Heritage',
+    activities: [
+      {
+        id: 'lake_zurich',
+        name: 'Lake Zurich',
+        imageUrl: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1b/aa/f7/69/every-lake-everywhere.jpg?w=1200&h=1200&s=1',
+        selected: false
+      },
+      {
+        id: 'interlaken',
+        name: 'Interlaken',
+        imageUrl: 'https://myswisspanorama.com/wp-content/uploads/2023/01/IMG_3565-1.jpg',
+        selected: false
+      },
+      {
+        id: 'mount_titlis',
+        name: 'Mount Titlis',
+        imageUrl: 'https://mediaim.expedia.com/localexpert/290251/874d0571-22d2-4de1-931e-890b0bbcd655.jpg',
+        selected: false
+      },
+      {
+        id: 'mount_jungfraujoch',
+        name: 'Mount Jungfraujoch (Highest Point of Europe)',
+        imageUrl: 'https://media.jungfrau.ch/image/upload/f_auto,fl_lossy,q_auto,c_crop,ar_1680:700/c_scale,w_3345/v1548413476/fileadmin/Jungfraujoch_Top_of_Europe/Jungfraujoch-Sphinx-Gletscher.jpg',
+        selected: false
+      },
+      {
+        id: 'lucerne',
+        name: 'Lucerne',
+        imageUrl: 'https://media.myswitzerland.com/image/fetch/c_lfill,g_auto,w_3200,h_1800/f_auto,q_80,fl_keep_iptc/https://www.myswitzerland.com/-/media/st/gadmin/images/cities/winter/cities/st0035250_28410.jpg',
+        selected: false
+      }
+    ],
+    accommodationOptions: [
+      'Accomodation (3 Star)',
+      'Accomodation (4 Star)',
+      'Accomodation (5 Star)',
+      'Seafacing Villa',
+      'Airbnb'
+    ],
+    metadata: {
+      region: 'Switzerland',
+      popular: false,
+      idealFor: ['history', 'heritage']
+    }
+  },
+
+  {
+    id: 'milan',
+    name: 'Milan',
+    imageUrl: 'https://images.unsplash.com/photo-1610016302534-6f67f1c968d8?q=80&w=1075&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    shortDesc: "The fashion capital of the world",
+    activities: [
+      {
+        id: 'sightseeing',
+        name: 'Sightseeing Tour and Food Trail',
+        imageUrl: 'https://globaltravelescapades.com/wp-content/uploads/2023/03/hori-2.0-30-768x512.jpg',
+        selected: false
+      },
+      {
+        id: 'duomo',
+        name: 'Duomo of Milan',
+        imageUrl: 'https://www.flawlessmilano.com/wp-content/uploads/2017/10/duomo-milano-facciata.jpg',
+        selected: false
+      },
+      {
+        id: 'fashion_trail',
+        name: 'Milan Fashion Trail',
+        imageUrl: 'https://theimpression.com/wp-content/uploads/2023/07/Ferragamo-atm-024-scaled.jpg',
+        selected: false
+      }
+    ],
+    accommodationOptions: [
+      '3 Star Cruise',
+      '4 Star Cruise',
+    ],
+    metadata: {
+      region: 'nile',
+      popular: false,
+      idealFor: ['cruise', 'chilling']
+    }
+  },
+
+  {
+    id: 'venice',
+    name: 'Venice',
+    imageUrl: 'https://www.fodors.com/wp-content/uploads/2019/11/HERO_Venice__FloatingCityBuilt_iStock-986940360.jpg',
+    shortDesc: "Floating city of canals and timeless charm",
+    activities: [
+      {
+        id: 'gondola_ride',
+        name: 'Gondola Ride',
+        imageUrl: 'https://goitalytours.com/wp-content/uploads/2019/05/Venice_Gondola.jpg',
+        selected: false
+      },
+      {
+        id: 'piazza_san_marco',
+        name: 'Piazza San Marco',
+        imageUrl: 'https://deih43ym53wif.cloudfront.net/st-marks-basilica-square-venice-shutterstock_1372373447.jpg_b135365c75.jpg',
+        selected: false
+      },
+    ],
+    accommodationOptions: [
+      'Accomodation (3 Star)',
+      'Accomodation (4 Star)',
+      'Accomodation (5 Star)',
+      'Beach Resort',
+      'Villa'
+    ],
+    metadata: {
+      region: 'Italy',
+      popular: false,
+      idealFor: ['history', 'beaches']
+    }
+  },
+  {
+    id: 'rome',
+    name: 'Rome',
+    imageUrl: 'https://images.unsplash.com/photo-1552832230-c0197dd311b5?q=80&w=1096&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    shortDesc: "Eternal city meets modern Charm",
+    activities: [
+      {
+        id: 'colosseum',
+        name: 'Colosseum',
+        imageUrl: 'https://media.cntraveler.com/photos/58da7b6e1021b64be09794da/16:9/w_2560%2Cc_limit/GettyImages-177303568.jpg',
+        selected: false
+      },
+      {
+        id: 'roman_forum',
+        name: 'Roman Forum & Ancient City',
+        imageUrl: 'https://a.cdn-hotels.com/gdcs/production171/d1538/62ecef81-422e-4d03-aae2-09f6980cad06.jpg',
+        selected: false
+      },
+      {
+        id: 'vatican_city',
+        name: 'Vatican City',
+        imageUrl: 'https://www.tripsavvy.com/thmb/lJZxO6ouePt1WCQE8_0pRV5oDIk=/2121x1414/filters:fill(auto,1)/GettyImages-163295967-c09b537080784f64a7835b3dcf3a63d5.jpg',
+        selected: false
+      },
+      {
+        id: 'sistine_chapel',
+        name: 'Sistine Chapel',
+        imageUrl: 'https://www.thoughtco.com/thmb/NgpsByw7f0_s2rq4Yg5ZyN11qZE=/3872x2592/filters:fill(auto,1)/SistineChapel-57ffd66e5f9b5805c2ac4916.jpg',
+        selected: false
+      },
+    ],
+    accommodationOptions: [
+      'Accomodation (3 Star)',
+      'Accomodation (4 Star)',
+      'Accomodation (5 Star)',
+      'Beach Resort',
+      'Villa'
+    ],
+    metadata: {
+      region: 'Lower Egypt',
+      popular: false,
+      idealFor: ['history', 'beaches']
+    }
+  }
+];
+
+// Optional: Helper functions for data access
+export const getDestinationById = (id) => 
+  destinations.find(dest => dest.id === id);
+
+export const getPopularDestinations = () => 
+  destinations.filter(dest => dest.metadata.popular);
+
+export const getDestinationsByRegion = (region) => 
+  destinations.filter(dest => dest.metadata.region === region);
